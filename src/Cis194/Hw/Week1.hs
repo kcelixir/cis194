b3fe0169a47cad0b351e494a9d358adab5c341f3
aelath <jkobernik@opentable.com>
2015-01-20 00:00:20 -0500

    bourbon and double ipa help us achieve an A

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -5,19 +5,29 @@ module Cis194.Hw.Week1 where
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits = map (read . (:[])) . show
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev = reverse . toDigits
+
+doubleEveryOther1 :: [Integer] -> [Integer]
+doubleEveryOther1 [] = []
+doubleEveryOther1 (x:[]) = (x:[])
+doubleEveryOther1 (x:(y:zs)) = x : (y * 2) : doubleEveryOther1 zs
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther = reverse . doubleEveryOther1 . reverse
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits = sum . concatMap toDigits
+
+valid :: Integer -> Integer
+valid = sumDigits . doubleEveryOther . toDigits
 
 validate :: Integer -> Bool
-validate _ = False
+validate x
+       | (valid x) `mod` 10 == 0 = True
+       | (valid x) `mod` 10 /= 0 = False
 
 ---------------------
 -- Towers of Hanoi --
