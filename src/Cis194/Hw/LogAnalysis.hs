6c965f0b78e2a9f1e187702c7ce637e6e79b971c
Stephen Spalding <sspalding@opentable.com>
2015-02-02 00:08:23 -0500

    Add LogAnalysis Ex4 solution

--- src/Cis194/Hw/LogAnalysis.hs
+++ src/Cis194/Hw/LogAnalysis.hs
@@ -35,7 +35,8 @@ build :: [LogMessage] -> MessageTree
 build x = foldr insert Leaf (reverse x)
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder _ = []
+inOrder Leaf = []
+inOrder (Node l n r) = (inOrder l) ++ [n] ++ (inOrder r)
 
 -- whatWentWrong takes an unsorted list of LogMessages, and returns a list of the
 -- messages corresponding to any errors with a severity of 50 or greater,
