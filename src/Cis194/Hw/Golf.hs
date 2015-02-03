7092e0ccd6da214ae88dcbc9a7331165e2ffb456
Stephen Spalding <sspalding@opentable.com>
2015-02-03 17:31:56 -0500

    Initial Ex1 (Hopscotch) solution

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,7 +1,14 @@
 module Cis194.Hw.Golf where
+-- import Data.List.Split
+-- dr n = map head . chunk n
 
 skips :: [a] -> [[a]]
-skips _ = []
+skips l = [(everynth n l) | n <- [1..(length l)]]
+
+-- takes a list, l, and returns a new list with
+-- only every nth value
+everynth :: Int -> [a] -> [a]
+everynth n l = [l !! (i-1) | i <- [n,2*n..(length l)]]
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima _ = []
