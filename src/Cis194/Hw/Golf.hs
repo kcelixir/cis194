7768d9cb465eec49e32e74b8fb19a4214161e968
Stephen Spalding <sspalding@opentable.com>
2015-02-04 23:49:58 -0500

    Initial solutions for Golf pass tests

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -11,7 +11,23 @@ everynth :: Int -> [a] -> [a]
 everynth n l = [l !! (i-1) | i <- [n,2*n..(length l)]]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima _ = []
+localMaxima [] = []
+localMaxima [_] = []
+localMaxima [_,_] = []
+localMaxima (x:y:z:s)
+  | y > x && y > z = [y] ++ (localMaxima (y:z:s))
+  | otherwise    = localMaxima (y:z:s)
 
 histogram :: [Integer] -> String
-histogram _ = ""
+histogram a = (unlines (reverse (drawHist (hist a))))
+
+count x = length . filter (\y -> y == x)
+
+hist :: [Integer] -> [Int]
+hist a = [count n a | n <- [0..9]]
+
+drawHist :: [Int] -> [String]
+drawHist a =  ["==========\n0123456789"]++[drawRow n a | n <- [1..(maximum a)]]
+
+drawRow :: Int -> [Int] -> String
+drawRow n a = [if x >= n then '*' else ' ' | x <- a]
