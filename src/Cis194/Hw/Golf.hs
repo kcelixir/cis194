d2cfa023dd51bbb25e8abd14266135a41eb78a2e
Stephen Spalding <sspalding@opentable.com>
2015-02-08 16:41:41 -0500

    Rename function to single letters and add comments

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -2,14 +2,20 @@ module Cis194.Hw.Golf where
 -- import Data.List.Split
 -- dr n = map head . chunk n
 
+----------------
+-- Exercise 1 --
+----------------
 skips :: [a] -> [[a]]
-skips l = [(everynth n l) | n <- [1..(length l)]]
+skips l = [(s n l) | n <- [1..(length l)]]
 
+s :: Int -> [a] -> [a]
 -- takes a list, l, and returns a new list with
 -- only every nth value
-everynth :: Int -> [a] -> [a]
-everynth n l = [l !! (i-1) | i <- [n,2*n..(length l)]]
+s n l = [l !! (i-1) | i <- [n,2*n..(length l)]]
 
+----------------
+-- Exercise 2 --
+----------------
 localMaxima :: [Integer] -> [Integer]
 localMaxima [] = []
 localMaxima [_] = []
@@ -18,20 +24,26 @@ localMaxima (x:y:z:s)
   | y > x && y > z = [y] ++ (localMaxima (y:z:s))
   | otherwise    = localMaxima (y:z:s)
 
+----------------
 -- Exercise 3 --
+----------------
 histogram :: [Integer] -> String
--- histogram = unlines . reverse . drawHist . hist
-histogram = unlines . drawHist . hist
+histogram = unlines . f . g
 
-count x = length . filter (\y -> y == x)
-
-hist :: [Integer] -> [Int]
+g :: [Integer] -> [Int]
 -- Count the frequencies of each number and store in a list
-hist a = [count n a | n <- [0..9]]
+g a = [c n a | n <- [0..9]]
 
-drawHist :: [Int] -> [String]
--- drawHist a =  ["==========\n0123456789"]++[drawRow n a | n <- [1..(maximum a)]]
-drawHist a =  [drawRow n a | n <- [9,8..1],n<=maximum a] ++ ["==========\n0123456789"]
+c :: Integer -> [Integer] -> Int
+-- counts how many of item x are in a list
+c x = length . filter (\y -> y == x)
 
-drawRow :: Int -> [Int] -> String
-drawRow n a = [if x >= n then '*' else ' ' | x <- a]
+f :: [Int] -> [String]
+-- Draw histogram using a list, a, of frequencies
+f a =  [r n a | n <- [9,8..1], n <= maximum a] 
+    ++ ["==========\n0123456789"]
+
+r :: Int -> [Int] -> String
+-- Draw row n of the histogram, using list, a, of frequencies
+r n a = [if x >= n then '*' else ' ' | x <- a]
+
