21551ede4e756fdde0ebdddb5eb5d72fbb7c35cf
Chris Keathley <c@keathley.io>
2015-02-09 01:26:40 -0500

    Oh f-:bomb: check out the histogram

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,13 +1,15 @@
 module Cis194.Hw.Golf where
 
+import Data.List
+import Data.Ord
+
 -- Exercise 1
 skips :: [a] -> [[a]]
-skips s = map (\x -> takeEvery x s) [0..(length s)-1]
-
-takeEvery :: Int -> [a] -> [a]
-takeEvery n xs = case drop n xs of
-  (y:ys) -> y : takeEvery n ys
-  []     -> []
+skips s = map (\x -> takeEvery' x s) [0..(length s)-1]
+  where
+    takeEvery' n xs = case drop n xs of
+      (y:ys) -> y : takeEvery' n ys
+      []     -> []
 
 -- Exercise 2
 localMaxima :: [Integer] -> [Integer]
@@ -20,5 +22,31 @@ localMaxima lst =
       | y > x && y > z = y : localMaxima(y:z:ls)
       | otherwise      = localMaxima(y:z:ls)
 
-histogram :: [Integer] -> String
-histogram _ = ""
+-- Exercise 3
+histogram :: [Int] -> String
+histogram lst = rows lst ++ footer
+  where
+    footer = "==========\n0123456789\n"
+
+rows :: [Int] -> String
+rows lst = concatMap(\x -> (buildRow x f)) (reverse [1..m])
+  where
+    f = frequencies lst
+    m = maxRow (frequencies lst)
+
+frequencies :: [Int] -> [(Int, Int)]
+-- frequencies = map (\xs -> (head xs, length xs)) . group . sort
+frequencies lst = map (\x -> (x, (length (filter (==x) lst)))) [0..9]
+
+maxRow :: [(Int, Int)] -> Int
+maxRow lst = snd $ maximumBy (comparing snd) lst
+
+buildRow :: Int -> [(Int, Int)] -> String
+buildRow _ []     = "\n"
+buildRow x (t:ts) = pickChar x t ++ buildRow x ts
+
+pickChar :: Int -> (Int, Int) -> String
+pickChar x t =
+  if snd t >= x
+  then "*"
+  else " "
