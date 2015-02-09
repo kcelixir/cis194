d8437386a36a305c9b78804a17578dda542e74da
aelath <jkobernik@gmail.com>
2015-02-08 23:17:17 -0500

    LOCALLY MAXIMIZED

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -12,7 +12,11 @@ skips x = let y = [1..(length x)]
          in (map (\i -> sks 1 i x) y)
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima _ = []
+localMaxima [] = []
+localMaxima (x:y:[]) = []
+localMaxima (x:r@(y:z:zz))
+            | (y > x) && (y > z) = y:(localMaxima r)
+            | otherwise = localMaxima r
 
 histogram :: [Integer] -> String
 histogram _ = ""
