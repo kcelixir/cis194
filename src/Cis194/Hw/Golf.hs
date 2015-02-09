bdabc3236d9a384439ad42f9f2d6cd9e6ce991ab
Chris Keathley <c@keathley.io>
2015-02-09 00:05:58 -0500

    Complete skips and refactor localMaxima

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,10 +1,24 @@
 module Cis194.Hw.Golf where
 
+-- Exercise 1
 skips :: [a] -> [[a]]
-skips _ = []
+skips s = map (\x -> takeEvery x s) [0..(length s)-1]
 
+takeEvery :: Int -> [a] -> [a]
+takeEvery n xs = case drop n xs of
+  (y:ys) -> y : takeEvery n ys
+  []     -> []
+
+-- Exercise 2
 localMaxima :: [Integer] -> [Integer]
-localMaxima _ = []
+localMaxima lst =
+  if length lst >= 3
+  then buildList' lst
+  else []
+  where
+    buildList' (x:y:z:ls)
+      | y > x && y > z = y : localMaxima(y:z:ls)
+      | otherwise      = localMaxima(y:z:ls)
 
 histogram :: [Integer] -> String
 histogram _ = ""
