3f1423e8a734c890e823d56d781bc58fef120350
Stephen Spalding <sspalding@opentable.com>
2015-02-12 00:38:28 -0500

    Add solution for Week4 Ex1.2

--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -11,11 +11,16 @@ fun1' = product . map (subtract 2) . filter even
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
-fun2 n | even n = n + fun2 (n `div` 2)
-  | otherwise = fun2 (3 * n + 1)
+fun2 n | even n    = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' _ = 0
+fun2' = sum . filter even . takeWhile (>1) . iterate collatz
+  where collatz x = 
+    if even x then 
+      div x 2
+    else
+      x * 3 + 1
 
 data Tree a = Leaf
   | Node Integer (Tree a) a (Tree a)
