10e9e953b7f9c1171be78dcef7e7334f475e208e
Chris Keathley <c@keathley.io>
2015-02-16 17:38:50 -0500

    Finish step 1

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,9 +1,11 @@
-module Cis194.Hw.Calc where 
+module Cis194.Hw.Calc where
 
 import  Cis194.Hw.ExprT
 
 eval :: ExprT -> Integer
-eval _ = 0
+eval (Lit x) = x
+eval (Add x y) = (eval x) + (eval y)
+eval (Mul x y) = (eval x) * (eval y)
 
 evalStr :: String -> Maybe Integer
 evalStr _ = Nothing
