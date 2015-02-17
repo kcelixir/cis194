72bc004821459d743d1d55db49b583e6d21900ba
Alex Cruikshank <alex@carbonfive.com>
2015-02-16 20:35:19 -0500

    Balanced tree

--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -21,8 +21,21 @@ data Tree a = Leaf
   | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
-foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree :: Ord a => [a] -> Tree a
+foldTree = foldr insert Leaf
+  where
+    insert a Leaf = Node 1 Leaf a Leaf
+    insert a (Node _ left b right) | a > b = balance $ Node 1 left b (insert a right)
+    insert a (Node _ left b right) = balance $ Node 1 (insert a left) b right
+    balance n@(Node _ left v right)
+      | depth left > depth right + 1 = Node (depth left) (leftSide left) (value left) (Node (depth left - 1) (rightSide left) v right)
+      | depth left < depth right - 1 = Node (depth right) (Node (depth right - 1) left v (leftSide right)) (value right) (rightSide right)
+      | otherwise = Node ((1 + max (depth left) (depth right))) left v right
+    value (Node _ _ value _) = value
+    leftSide (Node _ left _ _) = left
+    rightSide (Node _ _ _ right) = right
+    depth (Node depth _ _ _) = depth
+    depth Leaf = 0
 
 xor :: [Bool] -> Bool
 xor _ = False
