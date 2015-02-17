15f914e40dc83eda696814408ddf4b8c33089a52
Neil Menne <nhm@neilmenne.com>
2015-02-16 23:34:17 -0500

    week 4

--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -7,7 +7,7 @@ fun1 (x:xs)
   | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
-fun1' _ = 0
+fun1' =  product . map (subtract 2) . filter even
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
@@ -15,20 +15,37 @@ fun2 n | even n = n + fun2 (n `div` 2)
   | otherwise = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' _ = 0
+fun2' = sum . filter even . takeWhile (>1) . iterate iter
+    where iter n = if even n
+                   then n `div` 2
+                   else (3 * n + 1)
 
 data Tree a = Leaf
   | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node h _ _ _) = h
+
+insert :: a -> Tree a -> Tree a
+insert a Leaf = Node 0 Leaf a Leaf
+insert a (Node _ l c r) = tree
+  where hl = height l
+        hr = height r 
+        n = if hl <= hr then (insert a l) else (insert a r)
+        h = height n + 1
+        tree = if hl <= hr then Node h n c r else Node h l c n
+
 foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree = foldr insert Leaf
 
 xor :: [Bool] -> Bool
-xor _ = False
+xor = foldr (/=) False
 
 map' :: (a -> b) -> [a] -> [b]
-map' _ _ = []
+map' f = foldr (\x acc -> f x : acc) []
 
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram _ = []
+sieveSundaram n = 2:[2*x+1 | x <- [1..n], not $ elem x removed]
+    where removed = [i + j + 2*i*j | j <- [1..n], i <- [1..j]]
