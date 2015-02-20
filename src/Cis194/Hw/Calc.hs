83db5a65c9cef9217b4060a8c896ba140d6c2357
Stephen Spalding <sspalding@opentable.com>
2015-02-19 20:12:09 -0500

    Make answers a bit more pointless

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -13,7 +13,7 @@ eval (Add x y) = (eval x) + (eval y)
 -- Exercise 2 --
 
 evalStr :: String -> Maybe Integer
-evalStr s = parseExp id (+) (*) s
+evalStr = parseExp id (+) (*)
 
 -- Exercise 3 --
 
@@ -23,31 +23,31 @@ class Expr a where
   add :: a -> a -> a
 
 instance Expr ExprT where
-  lit x = (Lit x)
-  mul x y = (Mul x y)
-  add x y = (Add x y)
+  lit = Lit
+  mul = Mul
+  add = Add
 
 -- Exercise 4 --
 
 instance Expr Integer where
   lit x = x
-  mul x y = (x*y)
-  add x y = (x+y)
+  mul = (*)
+  add = (+)
   
 instance Expr Bool where
-  lit x = (x>0)
-  mul x y = (x && y)
-  add x y = (x || y)
+  lit = (>0)
+  mul = (&&)
+  add = (||)
   
 instance Expr MinMax where
-  lit x = (MinMax x)
+  lit = MinMax
   mul (MinMax x) (MinMax y) = (MinMax (min x y))
   add (MinMax x) (MinMax y) = (MinMax (max x y))
   
 instance Expr Mod7 where
-  lit x = (Mod7 (mod x 7))
-  mul (Mod7 x) (Mod7 y) = (Mod7 (mod (x*y) 7))
-  add (Mod7 x) (Mod7 y) = (Mod7 (mod (x+y) 7))
+  lit = Mod7 . flip mod 7
+  mul (Mod7 x) (Mod7 y) = Mod7 (mod (x*y) 7)
+  add (Mod7 x) (Mod7 y) = Mod7 (mod (x+y) 7)
   
 newtype MinMax  = MinMax Integer deriving (Eq, Show)
 newtype Mod7    = Mod7 Integer deriving (Eq, Show)
