9200e1c45a92b07daeeaef4283de11059cd0b2c8
Chris Keathley <c@keathley.io>
2015-02-22 20:48:34 -0500

    finish exercises for Calc

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Cis194.Hw.Calc where
 
-import  Cis194.Hw.ExprT
-import  Cis194.Hw.Parser
+import Cis194.Hw.ExprT
+import Cis194.Hw.Parser
+import qualified Cis194.Hw.StackVM as StackVM
 
 -- Step 1
 
@@ -18,6 +21,7 @@ evalStr s = case (parseExp Lit Add Mul s) of
   _      -> Nothing
 
 -- Step 3
+
 class Expr a where
   lit :: Integer -> a
   add :: a -> a -> a
@@ -37,29 +41,41 @@ newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
 newtype Mod7 = Mod7 Integer deriving (Eq, Show)
 
 instance Expr Integer where
-  lit x   = x
-  add x y = x + y
-  mul x y = x * y
+  lit = id
+  add = (+)
+  mul = (*)
 
 instance Expr Bool where
-  lit x
-    | x <= 0    = False
-    | otherwise = True
-  add x y = x || y
-  mul x y = x && y
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
 
 instance Expr MinMax where
-  lit x   = MinMax x
-  add x y = max x y
-  mul x y = min x y
+  lit x = MinMax x
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
 
 instance Expr Mod7 where
-  lit x   = Mod7 (x `mod` 7)
-  add (Mod7 x) (Mod7 y) = Mod7 (x + y)
-  mul x y = Mod7 (x * y)
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
 
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
 
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
 
+-- Exercise 5
 
+instance Expr StackVM.Program where
+  lit x   = (StackVM.PushI x) : []
+  add x y = x ++ y ++ [StackVM.Add]
+  mul x y = x ++ y ++ [StackVM.Mul]
 
-
+compile :: String -> Maybe StackVM.Program
+compile s = case (parseExp lit add mul s) of
+  Just x -> Just x
+  _      -> Nothing
