983d42639c307a41fef57da1245ea3aac8560ee0
Neil Menne <nhm@neilmenne.com>
2015-02-23 00:26:25 -0500

    week 5 part 2: doing exercise 6

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Cis194.Hw.Calc where 
 
 import Cis194.Hw.ExprT
 import Cis194.Hw.Parser
+-- For more information on how to use Data.Map, follow the link:
+-- https://hackage.haskell.org/package/containers-0.4.0.0/docs/Data-Map.html
+import qualified Data.Map as M
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
@@ -56,3 +60,45 @@ testInteger = testExp :: Maybe Integer
 testBool = testExp :: Maybe Bool
 testMM = testExp :: Maybe MinMax
 testSat = testExp :: Maybe Mod7
+
+class HasVars a where
+    var :: String -> a
+
+data VarExprT = VarLit Integer
+              | VarAdd VarExprT VarExprT
+              | VarMul VarExprT VarExprT
+              | Var String
+  deriving (Eq, Show)
+
+instance Expr VarExprT where
+    lit x = VarLit x
+    add x y = VarAdd x y
+    mul x y = VarMul x y
+
+instance HasVars VarExprT where
+    var s = Var s
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+    var s = M.lookup s
+
+-- only apply f if x and y are not nothing
+maybeF :: (a -> a -> a) -> Maybe a -> Maybe a -> Maybe a
+maybeF f (Just x) (Just y) = Just (f x y)
+maybeF _ _ _ = Nothing
+
+instance Expr (M.Map String Integer -> Maybe Integer) where
+    lit x = \_ -> Just x
+    add x y = \m -> maybeF (+) (x m) (y m)
+    mul x y = \m -> maybeF (*) (x m) (y m)
+
+withVars :: [(String, Integer)]
+         -> (M.Map String Integer -> Maybe Integer)
+         -> Maybe Integer
+withVars vs exp = exp $ M.fromList vs
+
+{-
+  test with the following from the assignment:
+  withVars [("x", 6)] $ add (lit 3) (var "x") - Just 9
+  withVars [("x", 6)] $ add (lit 3) (var "y") = Nothing -- because "y" is not in the map
+  withVars [("x", 6), ("y", 3)] $ mul (var "x") (add (var "y") (var "x")) == Just 54
+-}
