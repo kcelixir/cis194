5e5d6b5c7ad2e0e49b520c2ebaacd6c14ecefea1
Stephen Spalding <sspalding@opentable.com>
2015-04-05 00:49:22 -0400

    Risk Ex1 and Ex2

--- src/Cis194/Hw/Risk.hs
+++ src/Cis194/Hw/Risk.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Risk where
+module Cis194.Hw.Risk where
 
 import Control.Monad.Random
 
 ------------------------------------------------------------
 -- Die values
 
-newtype DieValue = DV { unDV :: Int } 
+newtype DieValue = DV { unDV :: Int }
   deriving (Eq, Ord, Show, Num)
 
 first :: (a -> b) -> (a, c) -> (b, c)
@@ -22,7 +22,25 @@ die = getRandom
 
 ------------------------------------------------------------
 -- Risk
-
+-- Ex2 --
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+dice ::  Army -> Rand StdGen [DieValue]
+dice n = sequence (replicate n die)
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle b = do
+  diceA <- dice $ min 3 (attackers b - 1)
+  diceD <- dice $ min 2 (defenders b)
+  return $ foldl attack b $ zip diceA diceD
+
+-- Returns result of single pair of dice
+attack :: Battlefield -> (DieValue,DieValue) -> Battlefield
+attack b (dieA, dieD)
+  | dieA > dieD = b {defenders = defenders b - 1}
+  | otherwise   = b {attackers = attackers b - 1}
+
+-- main :: IO ()
+-- main = readFile "data/company.txt" >>= putStr . guestList . read
