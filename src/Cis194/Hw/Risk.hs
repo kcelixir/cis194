e1a03df71bd5fd8c63c4940dbbe58ea287dbda99
Stephen Spalding <sspalding@opentable.com>
2015-04-06 00:19:55 -0400

    Risk Ex4

--- src/Cis194/Hw/Risk.hs
+++ src/Cis194/Hw/Risk.hs
@@ -29,29 +29,41 @@ type Army = Int
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
   deriving (Show)
 
-dice ::  Army -> Rand StdGen [DieValue]
 -- Rolls n dice
+dice ::  Army -> Rand StdGen [DieValue]
 dice n = sequence $ replicate n die
 
 -- Returns result of single pair of dice
 attack :: Battlefield -> (DieValue,DieValue) -> Battlefield
+attack b (dieA, dieD)
+  | dieA > dieD = b {defenders = defenders b - 1}
+  | otherwise   = b {attackers = attackers b - 1}
+
 battle :: Battlefield -> Rand StdGen Battlefield
 battle b = do
   diceA <- dice $ min 3 (attackers b - 1)
   diceD <- dice $ min 2 (defenders b)
   return $ foldl attack b $ zip (sort diceA) (sort diceD)
 
-attack b (dieA, dieD)
-  | dieA > dieD = b {defenders = defenders b - 1}
-  | otherwise   = b {attackers = attackers b - 1}
-
 -- Ex3 --
 invade :: Battlefield -> Rand StdGen Battlefield
-invade b
-  | attackers b < 2 || defenders b == 0 = return b
-  | otherwise = battle b >>= invade
+invade b@(Battlefield a d)
+  | a < 2 || d == 0 = return b
+  | otherwise       = battle b >>= invade
+
+-- Ex4 --
+successProb :: Battlefield -> Rand StdGen Double
+successProb b = do
+  inv <- invasions 1000 b
+  return $ (foldl successTab 0.0 inv)
+
+successTab :: Double -> Battlefield -> Double
+successTab p (Battlefield a d) = if d==0 then p + 0.001 else p
+
+invasions ::  Int -> Battlefield -> Rand StdGen [Battlefield]
+invasions n b = sequence $ replicate n $ invade b
 
 main :: IO ()
 main = do
-  res <- evalRandIO $ invade $ Battlefield 5 5
+  res <- evalRandIO $ successProb $ Battlefield 5 5
   putStrLn (show res)
