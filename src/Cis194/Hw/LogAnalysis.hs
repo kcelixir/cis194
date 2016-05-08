b3a93383414db2ceef6e1d50fa5557c5cd764a5d
Chris Keathley <c@keathley.io>
2016-05-08 00:02:01 -0400

    Week 2

--- src/Cis194/Hw/LogAnalysis.hs
+++ src/Cis194/Hw/LogAnalysis.hs
@@ -6,22 +6,44 @@ module Cis194.Hw.LogAnalysis where
 import  Cis194.Hw.Log
 
 parseMessage :: String -> LogMessage
-parseMessage s = Unknown s
+parseMessage s =
+  case words s of
+    "E":severity:time:message ->
+      LogMessage (Error (read severity)) (read time) (unwords message)
+    "I":time:message ->
+      LogMessage Info (read time) (unwords message)
+    "W":time:message ->
+      LogMessage Warning (read time) (unwords message)
+    message ->
+      Unknown (unwords message)
 
 parse :: String -> [LogMessage]
-parse _ = []
+parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
+insert lm Leaf = Node Leaf lm Leaf
+insert x@(LogMessage _ xTime _) (Node l y@(LogMessage _ yTime _) r)
+  | xTime < yTime = Node (insert x l) y r
+  | xTime > yTime = Node l y (insert x r)
 insert _ t = t
 
 build :: [LogMessage] -> MessageTree
-build _ = Leaf
+build = foldr insert Leaf
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder _ = []
+inOrder Leaf = []
+inOrder (Node l x r) = inOrder l ++ [x] ++ inOrder r
 
 -- whatWentWrong takes an unsorted list of LogMessages, and returns a list of the
 -- messages corresponding to any errors with a severity of 50 or greater,
 -- sorted by timestamp.
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong _ = []
+whatWentWrong = map messageText . filter isSevere . inOrder . build
+
+isSevere :: LogMessage -> Bool
+isSevere (LogMessage (Error severity) _ _) = severity >= 50
+isSevere _ = False
+
+messageText :: LogMessage -> String
+messageText (LogMessage _ _ m) = m
+messageText (Unknown m) = m
