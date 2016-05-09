29e968aa239fa4093fc514329ba5f23dce5b740c
Josh Ziegler <space-pope@users.noreply.github.com>
2016-05-09 00:21:55 -0400

    ex 1-4

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -1,33 +1,37 @@
-module Cis194.Hw.Week1 where
-
--------------
--- Ex 1-4  --
--------------
+import           Data.Char
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits n
+  | n < 0 = []
+  | otherwise = asIntegers (show n)
+
+asIntegers :: String -> [Integer]
+asIntegers s
+  | s == "" = []
+  | otherwise = toInteger (digitToInt (head s)) : asIntegers (tail s)
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev n = reverse (toDigits n)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther [x,y] = [2 * x, y]
+doubleEveryOther xs = do
+  let h = length xs - 2
+  let t = drop h xs
+  doubleEveryOther (take h xs) ++ doubleEveryOther t
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits [] = 0
+sumDigits [x] = sum (toDigits x)
+sumDigits (x:xs) = sumDigits [x] + sumDigits xs
 
 validate :: Integer -> Bool
-validate _ = False
-
----------------------
--- Towers of Hanoi --
----------------------
-
-type Peg = String
-type Move = (Peg, Peg)
-
-hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi _ _ _ _ = []
-
-hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
-hanoi4 _ _ _ _ _ = []
+validate n
+  | length (toDigits n) /= 16 = False
+  | otherwise = do
+    let ds = toDigits n
+    let doubled = doubleEveryOther ds
+    let summed = sumDigits doubled
+    summed `mod` 10 == 0
