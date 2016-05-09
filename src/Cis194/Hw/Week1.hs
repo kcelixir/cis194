c3845ec249a434d1fb5ed98f3d2ac08a5aaecb8d
Neil Menne <nmenne@opentable.com>
2016-05-09 13:56:45 -0400

    address linting feedback

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -10,20 +10,20 @@ toDigits x = reverse (toDigitsRev x)
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev x
     | x <= 0 = []
-    | otherwise = (mod x 10) : toDigitsRev (div x 10)
+    | otherwise = mod x 10 : toDigitsRev (div x 10)
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther (x1:x2:xs) = (x1 * 2) : x2 : doubleEveryOther xs
 doubleEveryOther x = x
 
 sumDigits :: [Integer] -> Integer
-sumDigits = (foldr (+) 0) -- or sum
+sumDigits = sum
 
 composeInt :: Integer -> Integer
 composeInt = sumDigits . concatMap toDigitsRev . doubleEveryOther . toDigits
 
 validate :: Integer -> Bool
-validate x = (mod (composeInt x) 10) == 0
+validate x = composeInt x `mod` 10 == 0
 
 ---------------------
 -- Towers of Hanoi --
@@ -34,9 +34,7 @@ type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
-hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
--- according to the HW:
--- length (hanoi 15 "a" "b" "c") == 32767
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 _ _ _ _ _ = []
