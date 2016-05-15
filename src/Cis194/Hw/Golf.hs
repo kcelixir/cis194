ef26bd0084c1c8aab9aed4361a166f31dbbf820f
Chris Keathley <c@keathley.io>
2016-05-15 13:39:37 -0400

    Week 3 :rocket:

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,10 +1,18 @@
 module Cis194.Hw.Golf where
 
+import Data.List
+
 skips :: [a] -> [[a]]
-skips _ = []
+skips l = [ [l!!(i-1) | i <- [n, n*2..(length l)]] | n <- [1..(length l)] ]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima _ = []
+localMaxima l = [y | (x:y:z:_) <- tails l, x < y && y > z]
 
 histogram :: [Integer] -> String
-histogram _ = ""
+histogram l = unlines $ reverse $  ["0123456789"] ++ ["=========="] ++ map concat (d l)
+
+d :: [Integer] -> [[String]]
+d l = [[if i >= j then "*" else " " | i <- c l] | j <- [1..maximum $ c l]]
+
+c :: [Integer] -> [Int]
+c l = map length [filter (==j) l | j <- [0..9]]
