c6639415905b081a29ffced1cf3274868481d74a
Wil Wade <willmwade@gmail.com>
2016-05-19 19:20:11 -0400

    better

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,10 +1,10 @@
 module Cis194.Hw.Golf where
 
 skips :: [a] -> [[a]]
-skips s = [nth i s | i <- [1..length s]]
+skips s = map (nth s) [1..length s]
 
-nth :: Int -> [a] -> [a]
-nth n m = [c | (c, i) <- zip m (cycle [1..n]), i == n]
+nth :: [a] -> Int -> [a]
+nth m n = [c | (c, i) <- zip m (cycle [1..n]), i == n]
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima s = [b | (a, b, c) <- zip3 s (drop 1 s) (drop 2 s), a < b && b > c]
