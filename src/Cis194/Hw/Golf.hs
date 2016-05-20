24162b473ef1de2870654480243def6ec62f6f5f
Wil Wade <wwade@opentable.com>
2016-05-20 09:21:34 -0400

    433 according to tr -d '[:blank:]' < src/Cis194/Hw/Golf.hs | wc -c

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,18 +1,20 @@
 module Cis194.Hw.Golf where
 
+g = length
+
 skips :: [a] -> [[a]]
-skips s = map (h s) [1..length s]
+skips s = map (h s) [1..g s]
 
 h :: [a] -> Int -> [a]
 h m n = [c | (c, i) <- zip m (cycle [1..n]), i == n]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima s = [b | (a, b, c) <- zip3 s (drop 1 s) (drop 2 s), a < b && b > c]
+localMaxima s = [b | (a, b, c) <- zip3 s (tail s) (drop 2 s), a < b && b > c]
 
 histogram :: [Integer] -> String
-histogram l = unlines (map (t [(replicate (b - length n) ' ') ++ n | n <- m]) [1..b])
+histogram l = unlines (map (t [(replicate (b - g n) ' ') ++ n | n <- m]) [1..b])
     where m = [concat ["*" | x <-l, x == i] ++ "=" ++ show i| i <- [0..9]]
-          b = maximum (map length m)
+          b = maximum (map g m)
 
 t :: [String] -> Int -> String
 t l i = [ head (h s i) | s <- l]
