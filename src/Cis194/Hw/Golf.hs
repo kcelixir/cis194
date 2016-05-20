7efa464146dca6ba2862dc9a6433e736edcd3799
Wil Wade <wwade@opentable.com>
2016-05-20 09:16:39 -0400

    439 according to tr -d '[:blank:]' < src/Cis194/Hw/Golf.hs | wc -c

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -1,20 +1,18 @@
 module Cis194.Hw.Golf where
 
 skips :: [a] -> [[a]]
-skips s = map (nth s) [1..length s]
+skips s = map (h s) [1..length s]
 
-nth :: [a] -> Int -> [a]
-nth m n = [c | (c, i) <- zip m (cycle [1..n]), i == n]
+h :: [a] -> Int -> [a]
+h m n = [c | (c, i) <- zip m (cycle [1..n]), i == n]
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima s = [b | (a, b, c) <- zip3 s (drop 1 s) (drop 2 s), a < b && b > c]
 
 histogram :: [Integer] -> String
-histogram l = unlines (transpose [concat ((replicate (b - length n) " ") ++ n) | n <- m])
-    where m = [["*" | x <-l, x == i] ++ ["=", show i]| i <- [0..9]]
+histogram l = unlines (map (t [(replicate (b - length n) ' ') ++ n | n <- m]) [1..b])
+    where m = [concat ["*" | x <-l, x == i] ++ "=" ++ show i| i <- [0..9]]
           b = maximum (map length m)
 
-transpose :: [[a]] -> [[a]]
-transpose []             = []
-transpose ([]   : xss)   = transpose xss
-transpose ((x:xs) : xss) = (x : [h | (h:_) <- xss]) : transpose (xs : [ t | (_:t) <- xss])
+t :: [String] -> Int -> String
+t l i = [ head (h s i) | s <- l]
