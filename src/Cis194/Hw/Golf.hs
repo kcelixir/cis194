7e185d16911b5ee7de8c4f9cfb9884d5bc699d92
Wil Wade <willmwade@gmail.com>
2016-05-22 22:10:37 -0400

    277 or 251 not sure which...

--- src/Cis194/Hw/Golf.hs
+++ src/Cis194/Hw/Golf.hs
@@ -15,7 +15,7 @@ skips s = map (\n -> [c | (c, i) <- zip s $ cycle [1..n], i == n]) [1..g s]
 
 -- Zip all with offset ones and check
 localMaxima :: [Integer] -> [Integer]
-localMaxima s = [b | (a, b, c) <- zip3 s (tail s) (drop 2 s), a < b && b > c]
+localMaxima s = [b | (a, b, c) <- zip3 s (tail s) $ drop 2 s, a < b && b > c]
 
 -- map 0..9 over the list and filter to just == and for each *
 -- then pad to the max length and transpose and unlines
