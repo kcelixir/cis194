ee8ce0ff00f7e8b82cdf33b8ade30c8dd178ad8a
Wil Wade <willmwade@gmail.com>
2016-05-30 07:19:34 -0400

    Starting on Week 4

--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -7,7 +7,7 @@ fun1 (x:xs)
   | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
-fun1' _ = 0
+fun1' = product . map (\x -> x - 2) . filter even
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
@@ -15,17 +15,17 @@ fun2 n | even n = n + fun2 (n `div` 2)
   | otherwise = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
-fun2' _ = 0
+fun2' _ = sum . takeWhile (>1) . iterate
 
 data Tree a = Leaf
   | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
 foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree _ = Leaf --foldr (\x acc -> ) Leaf 
 
 xor :: [Bool] -> Bool
-xor _ = False
+xor = odd . foldr (\x acc -> if x then acc + 1 else acc) 0
 
 map' :: (a -> b) -> [a] -> [b]
 map' _ _ = []
