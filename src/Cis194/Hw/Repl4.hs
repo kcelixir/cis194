6e169de9ecf095586ee3bb9c443737cae029fe67
Brent Spell <bspell@opentable.com>
2016-05-31 08:07:14 -0400

    week 4

---
+++ src/Cis194/Hw/Repl4.hs
@@ -0,0 +1,6 @@
+:set -i../../
+:set prompt "> "
+:l Week4.hs
+:l ../../../test/Cis194/Hw/Week4Spec.hs
+
+main
--- src/Cis194/Hw/Week4.hs
+++ src/Cis194/Hw/Week4.hs
@@ -7,28 +7,53 @@ fun1 (x:xs)
   | otherwise = fun1 xs
 
 fun1' :: [Integer] -> Integer
-fun1' _ = 0
+fun1' = foldr (*) 1 . map (subtract 2) . filter even
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
-fun2 n | even n = n + fun2 (n `div` 2)
+fun2 n
+  | even n = n + fun2 (n `div` 2)
   | otherwise = fun2 (3 * n + 1)
 
+fun2'' :: Integer -> Integer
+fun2'' n
+   |    even n = n `div` 2
+   | otherwise = 3 * n + 1
+
 fun2' :: Integer -> Integer
-fun2' _ = 0
+fun2' = sum . filter even . takeWhile (>1) . iterate fun2''
 
 data Tree a = Leaf
-  | Node Integer (Tree a) a (Tree a)
+            | Node Integer (Tree a) a (Tree a)
   deriving (Show, Eq)
 
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node h _ _ _) = h
+
+treeFold :: a -> Tree a -> Tree a
+treeFold a Leaf = Node 0 Leaf a Leaf
+treeFold a (Node h l n r)
+   | height l <= height r = let nl = treeFold a l
+                            in Node (height nl + 1) nl n r
+   |            otherwise = let nr = treeFold a r
+                            in Node (height nr + 1) l n nr
+
 foldTree :: [a] -> Tree a
-foldTree _ = Leaf
+foldTree = foldr treeFold Leaf
 
 xor :: [Bool] -> Bool
-xor _ = False
+xor = foldr (\x y -> (x || y) && not (x && y)) False
 
 map' :: (a -> b) -> [a] -> [b]
-map' _ _ = []
+map' f = foldr ((:) . f) []
 
 sieveSundaram :: Integer -> [Integer]
-sieveSundaram _ = []
+sieveSundaram n
+   |     n < 2 = []
+   | otherwise = 2:[2*x + 1 |
+                    x <- [1..n],
+                    not (x `elem` [i + j + 2*i*j |
+                                   j <- [1..n],
+                                   i <- [1..j],
+                                   i + j + 2*i*j <= n])]
