fbf56437f9f317032b0c8ca099b70215fecc2d7d
Chris Keathley <c@keathley.io>
2016-06-06 11:41:34 -0400

    Week 5 homework.

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,9 +1,56 @@
-module Cis194.Hw.Calc where 
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Cis194.Hw.Calc where
 
 import  Cis194.Hw.ExprT
+import  Cis194.Hw.Parser
 
 eval :: ExprT -> Integer
-eval _ = 0
+eval (Lit x) = x
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
 
 evalStr :: String -> Maybe Integer
-evalStr _ = Nothing
+evalStr str =
+  case parseExp Lit Mul Add str of
+    Just expr -> Just $ eval expr
+    Nothing -> Nothing
+
+newtype MinMax = MinMax Integer
+  deriving (Eq, Show)
+
+newtype Mod7 = Mod7 Integer
+  deriving (Eq, Show)
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = lit $ max x y
+  mul (MinMax x) (MinMax y) = lit $ min x y
+
+instance Expr Mod7 where
+  lit x = Mod7 $ x `mod` 7
+  add (Mod7 x) (Mod7 y) = lit $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = lit $ (x * y) `mod` 7
+
+compile :: String -> Maybe Program
+compile = case
