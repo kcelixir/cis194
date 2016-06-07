03bf49c7c10f26bedb547b2304e6fd91ae3f2436
Chris Keathley <c@keathley.io>
2016-06-06 20:42:12 -0400

    Finish exercises 5 and 6

--- src/Cis194/Hw/Calc.hs
+++ src/Cis194/Hw/Calc.hs
@@ -1,20 +1,23 @@
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Cis194.Hw.Calc where
 
-import  Cis194.Hw.ExprT
-import  Cis194.Hw.Parser
+import Cis194.Hw.Parser
+-- import Cis194.Hw.ExprT
+import qualified Cis194.Hw.StackVM as StackVM
+import qualified Data.Map as M
 
-eval :: ExprT -> Integer
-eval (Lit x) = x
-eval (Add x y) = eval x + eval y
-eval (Mul x y) = eval x * eval y
+-- eval :: ExprT -> Integer
+-- eval (Lit x) = x
+-- eval (Add x y) = eval x + eval y
+-- eval (Mul x y) = eval x * eval y
 
-evalStr :: String -> Maybe Integer
-evalStr str =
-  case parseExp Lit Mul Add str of
-    Just expr -> Just $ eval expr
-    Nothing -> Nothing
+-- evalStr :: String -> Maybe Integer
+-- evalStr str =
+--   case parseExp Lit Mul Add str of
+--     Just expr -> Just $ eval expr
+--     Nothing -> Nothing
 
 newtype MinMax = MinMax Integer
   deriving (Eq, Show)
@@ -27,10 +30,19 @@ class Expr a where
   add :: a -> a -> a
   mul :: a -> a -> a
 
-instance Expr ExprT where
-  lit = Lit
-  add = Add
-  mul = Mul
+class HasVars a where
+  var :: String -> a
+
+data VarExprT = Var String
+              | Lit Integer
+              | Add VarExprT VarExprT
+              | Mul VarExprT VarExprT
+  deriving (Show, Eq)
+
+-- instance Expr ExprT where
+--   lit = Lit
+--   add = Add
+--   mul = Mul
 
 instance Expr Integer where
   lit = id
@@ -52,5 +64,34 @@ instance Expr Mod7 where
   add (Mod7 x) (Mod7 y) = lit $ (x + y) `mod` 7
   mul (Mod7 x) (Mod7 y) = lit $ (x * y) `mod` 7
 
-compile :: String -> Maybe Program
-compile = case
+instance Expr StackVM.Program where
+  lit x   = [StackVM.PushI x]
+  add x y = x ++ y ++ [StackVM.Add]
+  mul x y = x ++ y ++ [StackVM.Mul]
+
+instance Expr VarExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+instance HasVars VarExprT where
+  var = Var
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+  var = M.lookup
+
+instance Expr (M.Map String Integer -> Maybe Integer) where
+  lit x _   = Just x
+  add x y m = (+) <$> x m <*> y m
+  mul x y m = (*) <$> x m <*> y m
+
+compile :: String -> Maybe StackVM.Program
+compile = parseExp lit add mul
+
+withVars :: [(String, Integer)]
+        -> (M.Map String Integer -> Maybe Integer)
+        -> Maybe Integer
+withVars vs expr = expr $ M.fromList vs
+
+test :: Maybe Integer
+test = withVars [("x", 6), ("y", 3)] $ mul (add (lit 3) (var "x")) (var "y")
