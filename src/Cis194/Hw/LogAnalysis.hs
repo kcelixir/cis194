a93524dce9b655f96bb64f1ec4c28e2e6360c057
Amos L King <amos@binarynoggin.com>
2018-11-20 10:45:24 -0600

    HW 2 init
    I have an issue with `buildMessage` not being exhaustive because it
    needs to match on an empty map. I really didn't like building all this
    stuff in the case statement and that is why I pulled it out. I think I
    need to start thinking of functions returning functions and build it up
    as arguments get passed in.
    
    Amos King @adkron <amos@binarynoggin.com>

--- src/Cis194/Hw/LogAnalysis.hs
+++ src/Cis194/Hw/LogAnalysis.hs
@@ -6,22 +6,44 @@ module Cis194.Hw.LogAnalysis where
 import  Cis194.Hw.Log
 
 parseMessage :: String -> LogMessage
-parseMessage s = Unknown s
+parseMessage s =
+  case words s of
+    "E":i:ws -> buildMessage (Error (read i)) ws
+    "I":ws -> buildMessage Info ws
+    "W":ws -> buildMessage Warning ws
+    _ -> Unknown s
+
+buildMessage :: MessageType -> [String] -> LogMessage
+buildMessage m (t:ws) = LogMessage m (read t) (unwords ws)
 
 parse :: String -> [LogMessage]
-parse _ = []
+parse = map (parseMessage) . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) t = t
+insert l Leaf = Node Leaf l Leaf
+insert n@(LogMessage _ nt _) (Node l m@(LogMessage _ mt _) r)
+  | nt < mt = Node (insert n l) m r
+  | nt > mt = Node l m (insert n r)
 insert _ t = t
 
 build :: [LogMessage] -> MessageTree
-build _ = Leaf
+build = foldr insert Leaf . reverse
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder _ = []
+inOrder (Node l m r) = inOrder l ++ [m] ++ inOrder r
+inOrder Leaf = []
 
 -- whatWentWrong takes an unsorted list of LogMessages, and returns a list of the
 -- messages corresponding to any errors with a severity of 50 or greater,
 -- sorted by timestamp.
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong _ = []
+whatWentWrong = map getString . filter isSever . inOrder . build
+
+isSever :: LogMessage -> Bool
+isSever (LogMessage (Error s) _ _) = s >= 50
+isSever _ = False
+
+getString :: LogMessage -> String
+getString (LogMessage _ _ s) = s
+getString (Unknown s) = s
