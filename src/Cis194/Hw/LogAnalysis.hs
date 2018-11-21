d1fce6f22c6bbac2ca4ab42d6c7420dab152c5a2
Amos L King <amos@binarynoggin.com>
2018-11-21 13:06:33 -0600

    Updates with some review items from @glguy
    I really like the pattern that was used in `inOrder` that was brilliant. Thanks
    for the help and the pointer on matching a type constructor.
    
    Amos King @adkron <amos@binarynoggin.com>

--- src/Cis194/Hw/LogAnalysis.hs
+++ src/Cis194/Hw/LogAnalysis.hs
@@ -8,19 +8,19 @@ import  Cis194.Hw.Log
 parseMessage :: String -> LogMessage
 parseMessage s =
   case words s of
-    "E":i:ws -> buildMessage (Error (read i)) ws
-    "I":ws -> buildMessage Info ws
-    "W":ws -> buildMessage Warning ws
+    "E":i:t:ws -> buildMessage (Error (read i)) t ws
+    "I":t:ws -> buildMessage Info t ws
+    "W":t:ws -> buildMessage Warning t ws
     _ -> Unknown s
 
-buildMessage :: MessageType -> [String] -> LogMessage
-buildMessage m (t:ws) = LogMessage m (read t) (unwords ws)
+buildMessage :: MessageType -> String -> [String] -> LogMessage
+buildMessage m t ws = LogMessage m (read t) (unwords ws)
 
 parse :: String -> [LogMessage]
-parse = map (parseMessage) . lines
+parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (Unknown _) t = t
+insert Unknown{} t = t
 insert l Leaf = Node Leaf l Leaf
 insert n@(LogMessage _ nt _) (Node l m@(LogMessage _ mt _) r)
   | nt < mt = Node (insert n l) m r
@@ -31,18 +31,20 @@ build :: [LogMessage] -> MessageTree
 build = foldr insert Leaf . reverse
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder (Node l m r) = inOrder l ++ [m] ++ inOrder r
-inOrder Leaf = []
+inOrder t = go t []
+  where
+    go (Node l m r) = go l . (m:) . go r
+    go Leaf = id
 
 -- whatWentWrong takes an unsorted list of LogMessages, and returns a list of the
 -- messages corresponding to any errors with a severity of 50 or greater,
 -- sorted by timestamp.
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = map getString . filter isSever . inOrder . build
+whatWentWrong = map getString . filter isSevere . inOrder . build
 
-isSever :: LogMessage -> Bool
-isSever (LogMessage (Error s) _ _) = s >= 50
-isSever _ = False
+isSevere :: LogMessage -> Bool
+isSevere (LogMessage (Error s) _ _) = s >= 50
+isSevere _ = False
 
 getString :: LogMessage -> String
 getString (LogMessage _ _ s) = s
