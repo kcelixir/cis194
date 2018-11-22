425eed4a86f2639d4e0ef5a1fbc37996637a2660
Dan Knutson <dan.knutson@gmail.com>
2018-11-21 20:26:22 -0600

    week one, part one

--- src/Cis194/Hw/Week1.hs
+++ src/Cis194/Hw/Week1.hs
@@ -1,23 +1,39 @@
 module Cis194.Hw.Week1 where
+import Data.Char
 
 -------------
 -- Ex 1-4  --
 -------------
 
 toDigits :: Integer -> [Integer]
-toDigits x = [x]
+toDigits x
+  | x < 1 = []
+  | otherwise = map (toInteger . digitToInt) (show x)
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = [x]
+toDigitsRev x = foldl (flip (:)) [] (toDigits x)
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = xs
+doubleEveryOther xs =
+  let len = fromIntegral (length xs)
+  in map (doubleMaybe len) (zip [0..] xs)
+
+doubleMaybe :: Integer -> (Integer, Integer) -> Integer
+doubleMaybe l (a, b)
+  | isDivisibleBy 2 (l - a) = b * 2
+  | otherwise = b
+
+isDivisibleBy :: Integer -> Integer -> Bool
+isDivisibleBy m x = mod x m == 0
 
 sumDigits :: [Integer] -> Integer
-sumDigits _ = 0
+sumDigits list = foldr (+) 0 (breakUpDigits list)
+
+breakUpDigits :: [Integer] -> [Integer]
+breakUpDigits list = list >>= toDigits
 
 validate :: Integer -> Bool
-validate _ = False
+validate = (isDivisibleBy 10) . sumDigits . doubleEveryOther . toDigits
 
 ---------------------
 -- Towers of Hanoi --
